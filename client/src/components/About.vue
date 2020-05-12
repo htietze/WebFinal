d220af284690c24d81a95d479be7600da348fc37
hugh <bup@bup.com>
2020-05-12 17:16:37 -0500

    adding leaflet

---
+++ client/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about">
+
+        <h2>Where have you traveled to?</h2>
+
+        <p>This site keeps track of how many states have been visited.</p>
+        <p>A Vue.js site by Hugh, following instructions</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
---
+++ client/src/router/index.js
@@ -0,0 +1,26 @@
+// associates base url path with statelist component?
+// sets home page?
+
+import Router from 'vue-router'
+
+import StateList from '@/components/StateList'
+import About from '@/components/About'
+import StateDetail from '@/components/StateDetail'
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            component: StateList
+        },
+        {
+            path: '/about',
+            component: About
+        },
+        {
+            path: '/detail/:state',
+            name: 'detail',
+            component: StateDetail
+        }
+    ]
+})
